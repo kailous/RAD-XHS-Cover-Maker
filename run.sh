694b6bb1d7d60b8ed24c81dde6f616245c85f326
Li Peng <kailous@live.cn>
2024-07-15 11:30:20 +0800

    Update run.sh
    支持参数输入 宽度参数 与目标文件
    中间生成的文件存放在临时文件夹 一并删除

--- run.sh
+++ run.sh
@@ -7,38 +7,54 @@ then
     exit 1
 fi
 
-# 检查是否提供了宽度参数
-if [ -z "$1" ]; then
-    echo "请提供输出文件的宽度。"
+# 获取宽度参数，默认为1200
+output_width="${1:-1200}"
+
+# 检查是否提供了图像文件名参数
+if [ -z "$2" ]; then
+    echo "请提供图像文件的名称。"
     exit 1
 fi
 
-# 获取宽度参数
-output_width="$1"
+# 获取图像文件名参数
+img="$2"
 
-# 定义图片文件名
-logo_on="logo_on.png"
-logo_off="logo_off.png"
-img="img.png"
-output1="01.png"
-output2="02.png"
-output3="03.png"
+# 创建隐藏的临时文件夹 .temp
+mkdir -p .temp
+
+# 定义图片文件名和URL
+logo_on_url="https://raw.githubusercontent.com/kailous/RAD-XHS-Cover-Maker/main/logo_on.png"
+logo_off_url="https://raw.githubusercontent.com/kailous/RAD-XHS-Cover-Maker/main/logo_off.png"
+
+# 定义本地文件名
+logo_on=".temp/logo_on.png"
+logo_off=".temp/logo_off.png"
+resized_logo_on=".temp/resized_logo_on.png"
+resized_logo_off=".temp/resized_logo_off.png"
+resized_img=".temp/resized_img.png"
+output1=".temp/01.png"
+output2=".temp/02.png"
+output3=".temp/03.png"
 output_webp="output.webp"
 output_zip="output.zip"
 
+# 下载图片文件
+curl -o "$logo_on" "$logo_on_url"
+curl -o "$logo_off" "$logo_off_url"
+
 # 等比缩放素材文件
-convert "$logo_on" -resize "${output_width}x" "resized_logo_on.png"
-convert "$logo_off" -resize "${output_width}x" "resized_logo_off.png"
-convert "$img" -resize "${output_width}x" "resized_img.png"
+convert "$logo_on" -resize "${output_width}x" "$resized_logo_on"
+convert "$logo_off" -resize "${output_width}x" "$resized_logo_off"
+convert "$img" -resize "${output_width}x" "$resized_img"
 echo "将素材文件等比缩放至宽度 $output_width"
 
 # 合并 resized_img.png 与 resized_logo_on.png 成为 01.png
-convert "resized_img.png" "resized_logo_on.png" -gravity center -composite "$output1"
-echo "合并 resized_img.png 和 resized_logo_on.png 成为 $output1"
+convert "$resized_img" "$resized_logo_on" -gravity center -composite "$output1"
+echo "合并 $resized_img 和 $resized_logo_on 成为 $output1"
 
 # 合并 resized_img.png 与 resized_logo_off.png 成为 02.png
-convert "resized_img.png" "resized_logo_off.png" -gravity center -composite "$output2"
-echo "合并 resized_img.png 和 resized_logo_off.png 成为 $output2"
+convert "$resized_img" "$resized_logo_off" -gravity center -composite "$output2"
+echo "合并 $resized_img 和 $resized_logo_off 成为 $output2"
 
 # 复制 02.png 为 03.png
 cp "$output2" "$output3"
@@ -52,8 +68,8 @@ echo "生成 $output_webp 动画"
 zip -j "$output_zip" "$output_webp" "$output1" "$output2" "$output3"
 echo "打包 $output_webp 和 PNG 文件成 $output_zip"
 
-# 删除临时文件和缩放后的 PNG 文件
-rm -f "$output1" "$output2" "$output3" "resized_logo_on.png" "resized_logo_off.png" "resized_img.png"
-echo "删除临时文件和缩放后的 PNG 文件"
+# 删除隐藏的临时文件夹 .temp
+rm -rf .temp
+echo "删除隐藏的临时文件夹 .temp"
 
 echo "操作完成。"
